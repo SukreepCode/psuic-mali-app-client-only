b31ab7fad649a8d62dee91e1be9bdeae4f17b553
Thada Wangthammang <mildronize@gmail.com>
2020-10-24 12:21:17 +0700

    disable typescript / check type manually by `type-check`

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,27 @@
-const CracoLessPlugin = require('craco-less');
+const CracoLessPlugin = require("craco-less");
 
 module.exports = {
+  // webpack: {
+  //   configure: (webpackConfig, { env, paths }) => {
+  //     const EXCLUDED_PLUGINS = ["ForkTsCheckerWebpackPlugin"];
+
+  //     webpackConfig.plugins = webpackConfig.plugins.filter(
+  //       (plugin) => !EXCLUDED_PLUGINS.includes(plugin.constructor.name)
+  //     );
+
+  //     return webpackConfig;
+  //   },
+  // },
+  typescript: {
+    enableTypeChecking: false /* (default value)  */,
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: {  },
+            modifyVars: {},
             javascriptEnabled: true,
           },
         },
